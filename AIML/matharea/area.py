c93b1930dd0c2f45a70885ba4ae3496d4e654f85
Rohit_kumar <101240542+Rohitkumar0107@users.noreply.github.com>
2023-05-06 10:54:41 +0530

    Add files via upload

---
+++ AIML/matharea/area.py
@@ -0,0 +1,15 @@
+# area.py
+''' module to calculate area'''
+
+# Function
+
+def AreaRec(lenght, bredth):
+    """area of rectangle """
+    return lenght*bredth
+
+def AreaCir(rad):
+    """area of circle """
+    return pi*rad
+
+# Constant
+pi=3.14
